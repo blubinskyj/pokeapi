ac97da2a907d936161ee0d539a9efffcb2bdac85
Bogdan <blubinskyj@gmail.com>
2024-01-30 18:47:54 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import styles from "./App.module.scss"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Quotes />
+        <span>
+          <span>Learn </span>
+          <a
+            className={styles.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className={styles.AppLink}
+            href="https://redux.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className={styles.AppLink}
+            href="https://redux-toolkit.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          <span>, </span>
+          <a
+            className={styles.AppLink}
+            href="https://react-redux.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+          ,<span> and </span>
+          <a
+            className={styles.AppLink}
+            href="https://reselect.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Reselect
+          </a>
+        </span>
+      </header>
+    </div>
+  )
+}
+
+export default App
